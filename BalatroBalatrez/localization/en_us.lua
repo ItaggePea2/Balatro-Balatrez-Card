c33aeb4655832cc3a9a96b4300ce1b6ceefa95a3
ItaggePea2 <ITaggePeaIa3@gmail.com>
2025-05-24 20:11:28 -0300

    Add files via upload

---
+++ BalatroBalatrez/localization/en_us.lua
@@ -0,0 +1,15 @@
+return {
+  descriptions = {
+    Joker = {
+      j_balatrez_balatro_balatrez = {
+        name = "Balatro Balatrez",
+        text = {
+          "This Joker gains {X:mult,C:white} X#2# {} Mult",
+          "y {X:chips,C:white} X#4#{} chips when", 
+          "a {C:attention}card{} is scored",
+          "{C:inactive}(Currently {X:mult,C:white} X#1# {C:inactive} Mult & {X:chips,C:white} X#3#{}{C:inactive}) Chips)",
+        }
+      }
+    }
+  }
+}
