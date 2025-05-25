c031e79f744bf75b45dbc65060196f9bdf6a2748
ItaggePea2 <ITaggePeaIa3@gmail.com>
2025-05-24 22:59:45 -0300

    1.0.1
    Fixed a typo where it said "Currently X1 Mult & X1) Chips)" instead of "Currently X1 Mult & X1 Chips)"

--- localization/en_us.lua
+++ localization/en_us.lua
@@ -7,7 +7,7 @@ return {
           "This Joker gains {X:mult,C:white} X#2# {} Mult",
           "y {X:chips,C:white} X#4#{} chips when", 
           "a {C:attention}card{} is scored",
-          "{C:inactive}(Currently {X:mult,C:white} X#1# {C:inactive} Mult & {X:chips,C:white} X#3#{}{C:inactive}) Chips)",
+          "{C:inactive}(Currently {X:mult,C:white} X#1# {C:inactive} Mult & {X:chips,C:white} X#3#{}{C:inactive} Chips)",
         }
       }
     }
